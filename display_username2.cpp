86df8944bbf3e8688445660ada5ee5e51ca41dca
shaileshyadav7771 <shaileshyadav7958@gmail.com>
2022-07-20 21:18:11 +0530

    C++ basic program..

---
+++ display_username2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string name;
+  cout << "My name is :";
+  cin >> name;
+  cout << "Welocome Mr : " <<name;
+  return 0;
+}
---
+++ first.cpp
@@ -0,0 +1,14 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a, b, c;
+    cout << "Enter 2 number : ";
+    cin >> a >> b;
+    c = a + b;
+    cout << "Addition of 2 number is: " << c;
+
+    return 0;
+}
